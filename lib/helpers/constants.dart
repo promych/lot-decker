fa55852ac9dfa014820a656b11c8636c3d0e3007
Roman <thetrusty@gmail.com>
2019-11-15 21:41:58 +0300

    add locales

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -2,25 +2,13 @@ import 'dart:ui' show Locale;
 
 const Map<String, Locale> kAppLocales = {
   'EN': const Locale('en', 'US'),
-  'DE': const Locale('de', 'DE')
+  'DE': const Locale('de', 'DE'),
+  'ES': const Locale('es', 'ES'),
+  'FR': const Locale('fr', 'FR'),
+  'IT': const Locale('it', 'IT'),
+  'JP': const Locale('ja', 'JP'),
+  'KR': const Locale('ko', 'KR'),
 };
-
-// const Map<String, String> kFactions = {
-//   'DE': 'Demacia',
-//   'FR': 'Freljord',
-//   'IO': 'Ionia',
-//   'NX': 'Noxus',
-//   'PZ': 'Piltover & Zaun',
-//   'SI': 'Shadow Isles',
-// };
-
-// const kCardTypes = {
-//   'types': ['Champion', 'Spell', 'Unit']
-// };
-
-// const kCardRarities = {
-//   'rarities': ['Champion', 'Rare', 'Epic', 'Common']
-// };
 
 const kMaxRegionsInDeck = 2;
 const kMaxSameCardsInDeck = 3;
